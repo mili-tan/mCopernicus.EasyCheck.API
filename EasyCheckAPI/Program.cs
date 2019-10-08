0b202c214680fb724211af5f97f61b2206ea71d5
Milkey Tan <819774989@qq.com>
2019-10-08 12:32:53 +0800

    添加项目文件。

---
+++ EasyCheckAPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace EasyCheckAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NancyHost host = new NancyHost(new HostConfiguration
+                    { RewriteLocalhost = true, UrlReservations = new UrlReservations { CreateAutomatically = true } },
+                new Uri("http://localhost:2020/")))
+            {
+                host.Start();
+
+                Console.WriteLine("EasyCheck is running on 2020 port");
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public sealed class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", x => {
+                return "hello";
+            });
+        }
+    }
+}
